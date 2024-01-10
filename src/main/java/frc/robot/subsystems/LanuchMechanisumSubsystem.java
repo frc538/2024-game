2f98f1251d8f6e279012ea4aa23ff03509bda7c6
dragonking1000 <zachariahjackson2018@outlook.com>
2024-01-10 15:28:56 -0600

    added motors for launcher mechanisum (kit bot)

---
+++ src/main/java/frc/robot/subsystems/LanuchMechanisumSubsystem.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class LanuchMechanisumSubsystem extends SubsystemBase {
+    PWMSparkMax topLaunch;
+    PWMSparkMax bottomLaunch;
+
+  /** Creates a new LanuchMechanisumSubsystem. */
+  public LanuchMechanisumSubsystem() {
+
+  topLaunch = new PWMSparkMax(Constants.PWM.topLaunch);
+  bottomLaunch = new PWMSparkMax(Constants.PWM.bottomLaunch);
+
+  }
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
