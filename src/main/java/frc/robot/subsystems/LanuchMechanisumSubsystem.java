ee0237ca2445d976f7a6b6b3d5e8de8c5bc5453c
dragonking1000 <zachariahjackson2018@outlook.com>
2024-02-08 19:22:50 -0600

    added an output to smart dash board for motor speeds

--- src/main/java/frc/robot/subsystems/LanuchMechanisumSubsystem.java
+++ src/main/java/frc/robot/subsystems/LanuchMechanisumSubsystem.java
@@ -8,7 +8,6 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.REVPhysicsSim;
 
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
