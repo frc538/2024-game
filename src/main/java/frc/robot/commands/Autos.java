5f6dcafe4f63441e9d808e7f35a0f4a2e8b3b560
Minecraftchest1 <minecraftchest1@outlook.com>
2024-02-19 12:11:20 -0600

    Uncomment imports in autos.

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -3,8 +3,8 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
-//import edu.wpi.first.wpilibj2.command.Command;
-//import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 
 public final class Autos {
 
