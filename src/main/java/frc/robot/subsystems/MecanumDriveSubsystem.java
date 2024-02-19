e27b1da597b9820d67112b08dbbb91097d26b669
dragonruler1000 <89262958+dragonruler1000@users.noreply.github.com>
2024-02-19 10:50:43 -0600

    changed rearRightriveMotor to rearRightDriveMotor

--- src/main/java/frc/robot/subsystems/MecanumDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/MecanumDriveSubsystem.java
@@ -24,7 +24,7 @@ public class MecanumDriveSubsystem extends SubsystemBase {
     frontLeft = new CANSparkMax(Constants.CANSparkMaxID.FrontLeftDriveMotor, MotorType.kBrushless);
     frontRight = new CANSparkMax(Constants.CANSparkMaxID.FrontRightDriveMotor, MotorType.kBrushless);
     rearLeft = new CANSparkMax(Constants.CANSparkMaxID.RearLeftDriveMotor, MotorType.kBrushless);
-    rearRight = new CANSparkMax(Constants.CANSparkMaxID.RearRightriveMotor, MotorType.kBrushless);
+    rearRight = new CANSparkMax(Constants.CANSparkMaxID.RearRighDriveMotor, MotorType.kBrushless);
 
     frontLeft.restoreFactoryDefaults();
     frontLeft.setInverted(false);
