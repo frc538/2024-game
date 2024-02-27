7354404ace007097bb06ea552cb7de68abdf5f06
OwenPatrik <145722866+OwenPatrik@users.noreply.github.com>
2024-02-26 18:18:08 -0600

    Swapped out drive odometry class for pose estimator class

--- src/main/java/frc/robot/subsystems/LimelightNavigation.java
+++ src/main/java/frc/robot/subsystems/LimelightNavigation.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import com.ctre.phoenix6.hardware.Pigeon2;
 import com.revrobotics.RelativeEncoder;
 
+import edu.wpi.first.math.estimator.MecanumDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -35,7 +36,8 @@ public class LimelightNavigation extends SubsystemBase {
   RelativeEncoder m_RearRightWheel_Encoder;
 
   MecanumDriveKinematics m_Kinematics;
-  MecanumDriveOdometry m_DriveOdometry;
+  //MecanumDriveOdometry m_DriveOdometry;
+  MecanumDrivePoseEstimator m_DrivePoseEstimator;
 
   private Pigeon2 pigeon2;
   //private Pigeon2Configuration pigeon2Config;
@@ -51,18 +53,16 @@ public class LimelightNavigation extends SubsystemBase {
     m_RearLeftWheel_Encoder		= Encoders.get("Rear Left");
     m_RearRightWheel_Encoder	= Encoders.get("Rear Right");
 
-
+    Pose2d initialPoseMeters = new Pose2d();
     m_Kinematics	= new MecanumDriveKinematics(Constants.Misc.FrontLeftDriveWheel_Position_Meters, Constants.Misc.FrontRightDriveWheel_Position_Meters,
 		Constants.Misc.RearLeftDriveWheel_Position_Meters, Constants.Misc.RearRightDriveWheel_Position_Meters);
-    m_DriveOdometry	= new MecanumDriveOdometry(m_Kinematics, new Rotation2d(0), new MecanumDriveWheelPositions(
+    m_DrivePoseEstimator	= new MecanumDrivePoseEstimator(m_Kinematics, new Rotation2d(0), new MecanumDriveWheelPositions(
     	m_FrontLeftWheel_Endocer.getPosition(), m_FrontRightWheel_Encoder.getPosition(), m_RearLeftWheel_Encoder.getPosition(), m_RearRightWheel_Encoder.getPosition()
-    ));
+    ),initialPoseMeters);
 
     pigeon2 = new Pigeon2(CanID.Pigeon2);
     //pigeon2Config = new Pigeon2Configuration();
 
-
-    x=0;
   }
 
   @Override
@@ -75,13 +75,11 @@ public class LimelightNavigation extends SubsystemBase {
 			m_FrontLeftWheel_Endocer.getPosition(), m_FrontRightWheel_Encoder.getPosition(), m_RearLeftWheel_Encoder.getPosition(), m_RearRightWheel_Encoder.getPosition()
 		);
 
-		m_DriveOdometry.resetPosition(pigeon2.getRotation2d(), MecanumDriveWheelPositions, robotPose2d);
+		m_DrivePoseEstimator.resetPosition(pigeon2.getRotation2d(), MecanumDriveWheelPositions, robotPose2d);
 	}
 
-	SmartDashboard.putNumber("Robot X", m_DriveOdometry.getPoseMeters().getX());
-	SmartDashboard.putNumber("Robot Y", m_DriveOdometry.getPoseMeters().getY());
-  SmartDashboard.putNumber("X", x);
-  x++;
+	SmartDashboard.putNumber("Robot X", m_DrivePoseEstimator.getEstimatedPosition().getX());
+	SmartDashboard.putNumber("Robot Y", m_DrivePoseEstimator.getEstimatedPosition().getY());
   }
 }
 
