1c4604864035cdf3c5cbcc5c55f409a2f0845593
dragonruler1000 <zachariahjackson2018@outlook.com>
2024-03-05 09:45:09 -0600

    removed unused imports

--- src/main/java/frc/robot/subsystems/climberSubsystem.java
+++ src/main/java/frc/robot/subsystems/climberSubsystem.java
@@ -5,8 +5,6 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.Pigeon2;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
