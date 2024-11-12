e7ae659a6d47bf6fc6ae4db3635beec58e3072ef
Nick Patrick <npatrick04@gmail.com>
2024-11-11 22:16:23 -0600

    Set default values to support replay

--- src/main/java/frc/robot/subsystems/Navigation/NavigationIO.java
+++ src/main/java/frc/robot/subsystems/Navigation/NavigationIO.java
@@ -10,27 +10,27 @@ public interface NavigationIO {
     
     @AutoLog
     public static class NavigationIOInputs {
-        Translation2d m_FrontLeftWheel_Position;
-        Translation2d m_FrontRightWheel_Position;
-        Translation2d m_RearLeftWheel_Position;
-        Translation2d m_RearRightWheel_Position;
+        Translation2d m_FrontLeftWheel_Position = new Translation2d(0.0,0.0);
+        Translation2d m_FrontRightWheel_Position = new Translation2d(0.0,0.0);
+        Translation2d m_RearLeftWheel_Position = new Translation2d(0.0,0.0);
+        Translation2d m_RearRightWheel_Position = new Translation2d(0.0,0.0);
 
         // Gyro IO
-        double gyroYaw;
-        double gyroPitch;
-        double gyroRoll;
-        Rotation2d gyroHeading;
+        double gyroYaw = 0.0;
+        double gyroPitch = 0.0;
+        double gyroRoll = 0.0;
+        Rotation2d gyroHeading = new Rotation2d(0.0);
 
         // Vision IO
-        boolean isVisionMeasurement;
-        Pose2d robotPose2d;
-        double captureLatency;
-        double pipelineLatency;
+        boolean isVisionMeasurement = false;
+        Pose2d robotPose2d = new Pose2d();
+        double captureLatency = 0.0;
+        double pipelineLatency = 0.0;
         int samplesCaptured = 0;
 
         // Application IO
-        Pose2d EstimatedPose2d;
-        double VisionLatency;
+        Pose2d EstimatedPose2d = new Pose2d();
+        double VisionLatency = 0.0;
     }
 
     public default void updateInputs(NavigationIOInputs io) {};
